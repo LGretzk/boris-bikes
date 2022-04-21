8378a4991997bec8fb53f768f67c355248d9d024
Luiza <luiza@Luizas-MacBook-Air.local>
2022-04-21 15:25:25 +0100

    Add dock_bike argument@

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,4 +10,19 @@ describe DockingStation do
             bike = subject.release_bike
             expect(bike.working?).to be_truthy
         end
+
+        it 'responds to dock_bike' do
+            expect(subject).to respond_to(:dock_bike).with(1).argument
+        end
+
+        it 'docks the bike passed on as argument' do
+            bike = Bike.new
+            expect(subject.dock_bike(bike)).to eq bike
+        end
+
+        it 'returns an array with a bike' do
+
+        end
+
+
     end
