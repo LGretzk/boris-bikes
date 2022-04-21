95b48097ad15084e36055c8c21581a0675c0f77c
Luiza <luiza@Luizas-MacBook-Air.local>
2022-04-21 14:21:22 +0100

    Refactor the code, chapter 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,17 +2,11 @@ require './lib/docking_station'
 
 describe DockingStation do
 
-    describe ".release_bike" do
-        @instance = DockingStation.new
-        @bike = Bike.new
-    
-        it "gets a bike" do
-            expect(@instance.release_bike).to eq(@bike)
+        it 'responds to release_bike' do
+            expect(subject).to respond_to :release_bike
         end
-    
-        it "checks if the bike is working" do 
-            expect(@bike.working?).to eq(true)
-        end
-    end
 
-end
+        #it "checks if the bike is working" do 
+         #   expect(@bike.working?).to eq(true)
+        #end
+    end
